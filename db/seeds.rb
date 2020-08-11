ffc837c3b4746e55f85466b84dfbe7d869aed696
Julien Victor <victor.julien84@gmail.com>
2020-08-11 16:51:50 +0200

    exercice 3 in course

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Article.create!(title: "Article 01", content: "content 01")
+Article.create!(title: "Article 02", content: "content 02")
+Article.create!(title: "Article 03", content: "content 03")
+Article.create!(title: "Article 04", content: "content 04")
+Article.create!(title: "Article 05", content: "content 05")
+Article.create!(title: "Article 06", content: "content 06")
+Article.create!(title: "Article 07", content: "content 07")
+Article.create!(title: "Article 08", content: "content 08")
+Article.create!(title: "Article 09", content: "content 09")
+Article.create!(title: "Article 10", content: "content 10")
